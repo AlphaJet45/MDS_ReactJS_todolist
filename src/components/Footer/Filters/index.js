e8d5c388c0672809ee4ef0ad04bf8e267eeb12e5
Damien <damien.m.45@gmail.com>
2019-05-28 10:17:02 +0200

    Fix filters and background for selected one

--- src/components/Footer/Filters/index.js
+++ src/components/Footer/Filters/index.js
@@ -2,14 +2,42 @@ import React from 'react';
 import './styles.css';
 
 class Filters extends React.Component {
+    state = {
+        selected: 'all',
+    }
+
+    handleFilter = event => filter => {
+        const { handleFilterAll, handleFilterTodo, handleFilterCompleted } = this.props;
+        console.log(filter, event);
+        if (filter === 'all') handleFilterAll(event);
+        if (filter === 'todo') handleFilterTodo(event);
+        if (filter === 'completed') handleFilterCompleted(event);
+
+        return this.setState({ selected: filter });
+    }
+
     render() {
-        const { handleFilterTodo, handleFilterCompleted, handleFilterAll } = this.props;
+        // const { handleFilterTodo, handleFilterCompleted, handleFilterAll } = this.props;
+        const { selected } = this.state;
+
         return (
             <div className="filters">
                 <ul>
-                    <li onClick={handleFilterAll}>All</li>
-                    <li onClick={handleFilterTodo}>Active</li>
-                    <li onClick={handleFilterCompleted}>Completed</li>
+                    <li className={selected === 'all' ? 'selected' : null}
+                        onClick={(event) => { 
+                            // const etape1 = handleFilterAll('all');
+                            // const etape2 = etape1(event);
+                            this.handleFilter(event)('all') }}>
+                        All
+                    </li>
+                    <li className={selected === 'todo' ? 'selected' : null}
+                        onClick={(event) => { this.handleFilter(event)('todo') }}>
+                        Active
+                    </li>
+                    <li className={selected === 'completed' ? 'selected' : null}
+                        onClick={(event) => { this.handleFilter(event)('completed') }}>
+                        Completed
+                    </li>
                 </ul>
             </div>
         );
